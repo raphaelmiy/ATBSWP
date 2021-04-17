25c6b826220923198dbd22575ac185c4f0b9a0f6
Raphael <raphaelmiyoyo14@gmail.com>
2021-04-17 17:20:10 +0300

    Add variables

---
+++ conditions.py
@@ -0,0 +1,65 @@
+import random
+
+#password = input()
+#if password == 'Raphael':
+#    print("Password is your name.")
+#elif password == "Raph":
+#    print("Password is the shortform of your name.")
+#else:
+#    print("Congratulations, the password is not your name.")
+
+# While Loops
+# spam = 0
+# while spam < 5:
+#     print("You are not a robot.")
+#     spam = spam + 1
+
+# Infinite Loops
+# name = ''
+# while name != 'your name':
+#     print('Please type of your name.')
+#     name = input()
+# print('Now you know what you are doing.')
+
+# Break Statements
+# while True:
+#     print('Please type your name.')
+#     name = input()
+#     if name == 'your name':
+#         print(200)
+#         break
+# print('300')
+
+# while True:
+#     print('Who are you?')
+#     name = input()
+#     if name != 'Joe':
+#         continue
+#     print("Hello Joe, what's your password? (Hint, its a fish)")
+#     password = input()
+#     if password == 'Swordfish':
+#         break
+# print('Access granted.')
+
+# name = ''
+# while not name:
+#     print('Enter your name.')
+#     name = input()
+# print('How many guests will you have')
+# num_of_guests = int(input())
+# if num_of_guests:
+#     print('Be sure to have enough room.')
+# print('Done')
+
+# print('My name is')
+# for i in range(5):
+#     print('Jimmy Five Times (' + str(i) + ')')
+
+# total = 0
+# for num in range(101):
+#     total = total + num
+# print(total)
+
+for i in range(5):
+    print(random.randint(23456, 2345678900987654))
+print(1600000/2000)
